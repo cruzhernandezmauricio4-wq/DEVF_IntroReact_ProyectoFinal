1cb92e2941dab8c455a3ac7329a5bd7af9e32968
Mauricio Cruz <cruzhernandezmauricio4@gmail.com>
2026-09-24 01:17:05 -0600

    agregar componentes de tweet

---
+++ src/components/Tweet.jsx
@@ -0,0 +1,10 @@
+const Tweet = ({ tweet, onLike }) => {
+  return (
+    <div className="tweet">
+      <p>{tweet.text}</p>
+      <button onClick={() => onLike(tweet.id)}>❤ {tweet.likes}</button>
+    </div>
+  );
+};
+
+export default Tweet;
